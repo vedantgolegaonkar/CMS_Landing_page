f16878d75a0e5300566527a1218a4f0dbd88b164
Vedant <vedantgolegaonkar@gmail.com>
2025-07-02 13:13:05 +0530

    hero section done

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -1,4 +1,11 @@
-import {HeroText1, HeroText2, subTitile1, subTitile2} from '../constants/HeroSection';
+import {
+  HeroText1,
+  HeroText2,
+  subTitile1,
+  subTitile2,
+  buttonText,
+} from '../constants/HeroSection';
+import { IoBookOutline } from 'react-icons/io5';
 
 const HeroSection = () => {
   return (
@@ -37,9 +44,38 @@ const HeroSection = () => {
           </h1>
         ))}
 
-        <div className='flex justify-center mt-12 space-x-10 '>
-          <button className='bg-white text-2xl text-blue-700 font-semibold px-4 py-2 rounded-lg'>Explore Courses</button>
-          <button className='bg-white text-2xl text-blue-700 font-semibold px-4 py-2 rounded-lg'>Explore Courses</button>
+        <div className="flex justify-center mt-12 space-x-10 ">
+          {buttonText.map((item) => (
+            <button
+              key={item.id}
+              className="flex items-center gap-2 bg-white text-2xl text-blue-700 font-semibold px-6 py-3 rounded-lg"
+            >
+              {' '}
+              <IoBookOutline /> {item.text}
+            </button>
+          ))}
+          <button className="bg-white text-2xl text-white font-semibold px-4 py-2 rounded-lg">
+            Explore Courses
+          </button>
+        </div>
+
+        <div className="flex justify-center mt-15 space-x-60 font-black text-white text-[40px]">
+          <div className="flex flex-col items-center">
+            <div>250K+</div>
+            <div className="text-xl font-medium ">Global Students</div>
+          </div>
+          <div className="flex flex-col items-center">
+            <div>500+</div>
+            <div className="text-xl font-medium ">Expert Courses</div>
+          </div>
+          <div className="flex flex-col items-center">
+            <div>50+</div>
+            <div className="text-xl font-medium ">Countries</div>
+          </div>
+          <div className="flex flex-col items-center">
+            <div>95%</div>
+            <div className="text-xl font-medium ">Success Rate</div>
+          </div>
         </div>
       </div>
     </>
--- constants/HeroSection.js
+++ constants/HeroSection.js
@@ -2,27 +2,34 @@ export const HeroText1 = [
   {
     id: 1,
     title: 'Master Global Finance',
-  }
+  },
 ];
 
 export const HeroText2 = [
   {
     id: 1,
     title: 'With FineEgde',
-  }
+  },
 ];
 
-export const subTitile1 =[
-    {
-        id:1,
-        title: "Empowering Chartered Accountants worldwide with cross-border expertise,"
-    }
-]
+export const subTitile1 = [
+  {
+    id: 1,
+    title:
+      'Empowering Chartered Accountants worldwide with cross-border expertise,',
+  },
+];
 
-export const subTitile2 =[
-    {
-        id:1,
-        title: "international certifications, and AI-powered learning"
-    }
-]
+export const subTitile2 = [
+  {
+    id: 1,
+    title: 'international certifications, and AI-powered learning',
+  },
+];
 
+export const buttonText = [
+  {
+    id: 1,
+    text: 'Explore Courses',
+  },
+];
